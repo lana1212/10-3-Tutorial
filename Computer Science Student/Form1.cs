990e8fc124c08c91aaae87415cda1e94086dbf43
Lana Karazoun <lkarazoun@usf.edu>
2021-03-22 17:30:24 -0400

    Add project files.

---
+++ Computer Science Student/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Computer_Science_Student
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void getHoursButton_Click(object sender, EventArgs e)
+        {
+            //variables to hold input 
+            string name, id, track;
+
+            //get student's name and id
+            name = nameTextBox.Text;
+            id = idTextBox.Text;
+
+            //get students academic track
+            if (infoSystemsRadioButton.Checked)
+            {
+                track = "Information Systems";
+            }
+            else
+            {
+                track = "Software Engineering";
+            }
+
+            //create a CompSciStudent object
+            CompSciStudent csStudent =
+                new CompSciStudent(name, id, track);
+
+            //display student's required hours 
+            requiredHoursLabel.Text = csStudent.RequiredHours.ToString("n1");
+        }
+
+        private void exitButton_Click(object sender, EventArgs e)
+        {
+            //close the form
+            this.Close();
+        }
+    }
+}
